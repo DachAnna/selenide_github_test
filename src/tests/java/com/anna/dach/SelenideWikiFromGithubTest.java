dce3222b3c80ed8a0bef77ed0f250a1e039e2977
Anna <sub.ekt@inbox.ru>
2022-12-07 16:08:37 +0300

    Add class DragAndDrop

--- src/tests/java/com/anna/dach/SelenideWikiFromGithubTest.java
+++ src/tests/java/com/anna/dach/SelenideWikiFromGithubTest.java
@@ -1,21 +1,18 @@
-package simbirsoft.dyachenkoav.test;
+package com.anna.dach;
 
 import static com.codeborne.selenide.Condition.*;
 import org.junit.jupiter.api.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SelenideWikiFromGithubTest {
+
     @Test
     void findExampleForJUnit5OnPageSelenideWiki(){
-        //Поиск и переход на страницу Selenide в Github
         open("https://github.com");
         //Ввести в поле поиска selenide и нажать Enter
         $("[data-test-selector=nav-search-input]").setValue("selenide").pressEnter();
-        //нажать на первую ссылку в выдаче поиска
         $$(".repo-list li").first().$("a").click();
-        //Проверить, что в заголовке встречается selenide/selenide
         $("h1").shouldHave(text("selenide/selenide"));
-        //Переход в раздел Wiki
         $("#wiki-tab").click();
         //Ракрыть полный список страниц Pages
         $$(".js-wiki-sidebar-toggle-display li").last().$("button").click();
