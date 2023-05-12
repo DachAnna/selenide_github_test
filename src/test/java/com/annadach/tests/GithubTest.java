769d82e37cf3069273bbbabf46ce21f9f347d4d5
Anna <sub.ekt@inbox.ru>
2023-05-13 00:21:06 +0300

    Add steps for testOps

---
+++ src/test/java/com/annadach/tests/GithubTest.java
@@ -0,0 +1,59 @@
+package com.annadach.tests;
+
+import static com.codeborne.selenide.Condition.*;
+
+import io.qameta.allure.AllureId;
+import io.qameta.allure.Owner;
+import org.junit.jupiter.api.*;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+
+@Owner("allure8")
+public class GithubTest {
+
+    @Test
+    @AllureId("18956")
+    @DisplayName("Поиск задачи в репозитории неавторизованным пользователем")
+    @Tags({@Tag("critical")})
+    void findExampleForJUnit5OnPageSelenideWiki(){
+        step("Открыть главную страницу", () -> {
+                open("https://github.com");
+        });
+
+        step("Найти репозиторий \"selenide\"", () -> {
+
+            step("В поле поиска в шапке ввести текст \"selenide\"", () -> {
+                $("[data-scoped-placeholder=Search]").setValue("selenide");
+            });
+
+            step("Кликнуть Enter", () -> {
+                $("[data-scoped-placeholder=Search]").pressEnter();
+            });
+
+            step("Перейти по первой ссылке selenide/selenide", () -> {
+                $$(".repo-list li").first().$("a").click();
+                $("h1").shouldHave(text("selenide/selenide"));
+            });
+        });
+
+        step("Открыть таб \"Wiki\"", () -> {
+            $("#wiki-tab").click();
+        });
+
+        step("Перейти на страницу SoftAssertions", () -> {
+
+            step("Открыть полный список страниц (Pages) кликом по тексту \"Show 2 more pages...\"", () -> {
+                $$(".js-wiki-sidebar-toggle-display li").last().$("button").click();
+                $$(".js-wiki-sidebar-toggle-display li summary").last().$("a").shouldHave(text("SoftAssertions"));
+            });
+
+            step("Кликнуть по ссылке SoftAssertions", () -> {
+                $$(".js-wiki-sidebar-toggle-display li summary").last().$("a").click();
+            });
+        });
+
+        step("Убедиться, что на странице присутствует текст \"Using JUnit5 extend test class\"", () -> {
+            $("#user-content-3-using-junit5-extend-test-class").parent().shouldHave(text("JUnit5"));
+        });
+    }
+}
