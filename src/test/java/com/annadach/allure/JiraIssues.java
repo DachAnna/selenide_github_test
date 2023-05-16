9e50a36f4ad5f3635331828b13866963a4adfab6
Anna <sub.ekt@inbox.ru>
2023-05-16 12:25:53 +0300

    Add allure test

---
+++ src/test/java/com/annadach/allure/JiraIssues.java
@@ -0,0 +1,12 @@
+package com.annadach.allure;
+
+import java.lang.annotation.*;
+
+@Documented
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
+public @interface JiraIssues {
+
+    JiraIssue[] value();
+}
--- src/test/java/com/annadach/tests/GithubTest.java
+++ src/test/java/com/annadach/tests/GithubTest.java
@@ -2,22 +2,30 @@ package com.annadach.tests;
 
 import static com.codeborne.selenide.Condition.*;
 
+import com.annadach.allure.Layer;
+import com.annadach.allure.Microservice;
 import io.qameta.allure.AllureId;
+import io.qameta.allure.Feature;
 import io.qameta.allure.Owner;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
 @Owner("allure8")
-public class GithubTest extends TestBase{
+@Layer("web")
+public class GithubTest extends TestBase {
 
     @Test
     @AllureId("18956")
     @DisplayName("Поиск задачи в репозитории неавторизованным пользователем")
-    @Tags({@Tag("critical")})
-    void findExampleForJUnit5OnPageSelenideWiki(){
+    @Tags({@Tag("critical"), @Tag("UI-tests")})
+    @Microservice("Issue")
+    @Feature("Поиск")
+    void findExampleForJUnit5OnPageSelenideWiki() {
         step("Открыть главную страницу", () -> {
-                open("/");
+            open("/");
         });
 
         step("Найти репозиторий \"selenide\"", () -> {
@@ -54,6 +62,46 @@ public class GithubTest extends TestBase{
 
         step("Убедиться, что на странице присутствует текст \"Using JUnit5 extend test class\"", () -> {
             $("#user-content-3-using-junit5-extend-test-class").parent().shouldHave(text("JUnit5"));
+        });
+    }
+
+    @Test
+    @AllureId("18951")
+    @DisplayName("Неавторизованный пользователь может найти задачу в репозитории")
+    @Owner("allure8")
+    @Tags({@Tag("critical"), @Tag("UI-tests")})
+    @Microservice("Issue")
+    @Feature("Поиск")
+    void SearchIssue() {
+        step("Открыть главную страницу", () -> {
+            open("/");
+        });
+
+        step("Найти репозиторий  allure-examples в форме поиска в шапке", () -> {
+
+            step("Кликнуть на поисковую строку", () -> {
+                $(By.name("q")).click();
+            });
+
+            step("Ввести текст  allure-examples", () -> {
+                $(By.name("q")).setValue("allure-examples");
+            });
+
+            step("Нажать кнопку Enter", () -> {
+                $(By.name("q")).pressEnter();
+            });
+        });
+
+        step("Перейти по первой ссылке с списке allure-examples/allure-examples", () -> {
+            $$(".repo-list li").first().$("a").click();
+        });
+
+        step("Перейти во вкладку Задачи (Issues)", () -> {
+            $("#issues-tab").click();
+        });
+
+        step("Проверить наличие задачи #38 на странице", () -> {
+            $(".js-navigation-container.js-active-navigation-container").shouldHave(text("#38"));
         });
     }
 }
